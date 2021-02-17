c1f39e37a6310350c0268a529e1468cb7ebc585a
ApexPredeator23 <72398756+ApexPredeator23@users.noreply.github.com>
2021-02-17 06:40:43 +0530

    Update Synchronization.java

--- Synchronization.java
+++ Synchronization.java
@@ -1,9 +1,38 @@
 package com.leranthread;
 
+class Counter {
+	int count;
+
+	public synchronized void increase() {
+		count++;
+	}
+}
+
 public class Synchronization {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws Exception {
+		Counter c1 = new Counter();
+		Thread t1 = new Thread(new Runnable() {
+			public void run() {
+				for (int i = 0; i < 1000; i++)
+					c1.increase();
+			}
+		});
+
+		Thread t2 = new Thread(new Runnable() {
+			public void run() {
+				for (int i = 0; i < 1000; i++)
+					c1.increase();
+			}
+		});
+
+		t1.start();
+		t2.start();
+
+		t1.join();
+		t2.join();
+
+		System.out.println("Count: " + c1.count);
 
 	}
 
